d3acba5bcf5dfd08c54edd9940b426623aeb147b
Patryk <gustaw5022@gmail.com>
2020-01-03 21:27:30 +0100

    created simple user authentification after login

--- app/src/main/java/com/e/booksrental_mobile/Activities/LoginActivity.kt
+++ app/src/main/java/com/e/booksrental_mobile/Activities/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.e.booksrental_mobile.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.e.booksrental_mobile.Activities.Admin.AdminMainMenu
+import com.e.booksrental_mobile.Activities.User.UserMainMenu
 import com.e.booksrental_mobile.R
 import kotlin.math.log
 
@@ -22,7 +25,22 @@ class LoginActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener{
             if(areFieldsFilled(loginEditText, passwordEditText)){
-
+                val userType: Int = checkUser(loginEditText.text.toString(),
+                    passwordEditText.text.toString())
+                if(userType == 3){
+                    Toast.makeText(this, "Podane dane są niepoprawne",
+                        Toast.LENGTH_SHORT).show()
+                }else{
+                    if(userType == 1){
+                        val intent = Intent(this, AdminMainMenu::class.java)
+                        startActivity(intent)
+                    }else if(userType == 2){
+                        val intent = Intent(this, UserMainMenu::class.java)
+                        startActivity(intent)
+                    }
+                }
+                loginEditText.text.clear()
+                passwordEditText.text.clear()
             }else{
                 Toast.makeText(this,"Wypełnij wszystkie pola", Toast.LENGTH_SHORT).show()
             }
@@ -43,9 +61,15 @@ class LoginActivity : AppCompatActivity() {
         return isCorrect
     }
 
-//    private fun checkUser(login: String, password: String): Int{
-//
-//    }
+    private fun checkUser(login: String, password: String): Int{
+        if(login.equals("admin") && password.equals("admin")){
+            return 1
+        }else if(login.equals("user") && password.equals("user")){
+            return 2
+        }else{
+            return 3
+        }
+    }
 
     override fun onBackPressed() {
 
