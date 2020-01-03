8fe6f8085674d5126145a014c03a458a6987008a
Patryk <gustaw5022@gmail.com>
2020-01-03 22:15:25 +0100

    added rentalListActivity

---
+++ app/src/main/java/com/e/booksrental_mobile/Activities/User/RentalListActivity.kt
@@ -0,0 +1,12 @@
+package com.e.booksrental_mobile.Activities.User
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RentalListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rental_list)
+    }
+}
