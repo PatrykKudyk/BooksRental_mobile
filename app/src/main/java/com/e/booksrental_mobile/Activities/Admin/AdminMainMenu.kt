1de713305d7422d624b0db5093bf1ad26c5053e9
Patryk <gustaw5022@gmail.com>
2020-01-03 21:27:55 +0100

    added almost empty user and admin menu

---
+++ app/src/main/java/com/e/booksrental_mobile/Activities/Admin/AdminMainMenu.kt
@@ -0,0 +1,17 @@
+package com.e.booksrental_mobile.Activities.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.booksrental_mobile.R
+
+class AdminMainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_main_menu)
+    }
+
+    override fun onBackPressed() {
+        
+    }
+}
---
+++ app/src/main/java/com/e/booksrental_mobile/Activities/User/UserMainMenu.kt
@@ -0,0 +1,17 @@
+package com.e.booksrental_mobile.Activities.User
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.booksrental_mobile.R
+
+class UserMainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_main_menu)
+    }
+
+    override fun onBackPressed() {
+
+    }
+}
