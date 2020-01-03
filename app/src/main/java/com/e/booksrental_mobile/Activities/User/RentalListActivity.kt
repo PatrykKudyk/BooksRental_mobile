d22ecae77fbc2351173ea9ccb295fa1cd5edbf95
Patryk <gustaw5022@gmail.com>
2020-01-03 22:24:22 +0100

    renamed button drawable background plus made minor changes in usermenu

--- app/src/main/java/com/e/booksrental_mobile/Activities/User/RentalListActivity.kt
+++ app/src/main/java/com/e/booksrental_mobile/Activities/User/RentalListActivity.kt
@@ -2,11 +2,16 @@ package com.e.booksrental_mobile.Activities.User
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.e.booksrental_mobile.R
 
 class RentalListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rental_list)
+    }
+
+    override fun onBackPressed() {
+
     }
 }
--- app/src/main/java/com/e/booksrental_mobile/Activities/User/UserMainMenu.kt
+++ app/src/main/java/com/e/booksrental_mobile/Activities/User/UserMainMenu.kt
@@ -1,7 +1,9 @@
 package com.e.booksrental_mobile.Activities.User
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.e.booksrental_mobile.R
 
 class UserMainMenu : AppCompatActivity() {
@@ -9,6 +11,24 @@ class UserMainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_main_menu)
+
+        val rentalBooksButton = this.findViewById(R.id.show_rentals_button) as Button
+        val authorBooksButton = this.findViewById(R.id.show_author_books_button) as Button
+        val booksByNameButton = this.findViewById(R.id.search_books_by_name_button) as Button
+
+        rentalBooksButton.setOnClickListener {
+            val intent = Intent(this, RentalListActivity::class.java)
+            startActivity(intent)
+        }
+
+        authorBooksButton.setOnClickListener {
+
+        }
+
+        booksByNameButton.setOnClickListener {
+
+        }
+
     }
 
     override fun onBackPressed() {
