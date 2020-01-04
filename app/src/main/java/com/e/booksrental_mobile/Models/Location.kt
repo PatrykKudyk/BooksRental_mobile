91530cdb016c96ef7cbd95ec56fb27ea8923bf84
Patryk <gustaw5022@gmail.com>
2020-01-04 17:48:03 +0100

    minor change in location model

--- app/src/main/java/com/e/booksrental_mobile/Models/Location.kt
+++ app/src/main/java/com/e/booksrental_mobile/Models/Location.kt
@@ -2,5 +2,5 @@ package com.e.booksrental_mobile.Models
 
 class Location(val id: Long, val state: String, val city: String, val zip_code: String,
                val street: String, val building_number: String, val office_number: String,
-               val is_used: Boolean, val publishing_house: PublishingHouse, val rental: Rental) {
+               val _used: Boolean, val publishing_house: PublishingHouse, val rental: Rental) {
 }
