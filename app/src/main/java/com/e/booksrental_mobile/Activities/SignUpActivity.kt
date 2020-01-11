ee72cfdcef8168b2e2422838c19f0c4ac63ca58f
Patryk <gustaw5022@gmail.com>
2020-01-11 13:21:19 +0100

    added simple correctness test in signup activity

--- app/src/main/java/com/e/booksrental_mobile/Activities/SignUpActivity.kt
+++ app/src/main/java/com/e/booksrental_mobile/Activities/SignUpActivity.kt
@@ -2,12 +2,59 @@ package com.e.booksrental_mobile.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.e.booksrental_mobile.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        register_button.setOnClickListener {
+            if(areAllFieldsFilled()){
+
+            }else{
+                Toast.makeText(this, "Wypełnij wszystkie pola", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        back_button.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun areAllFieldsFilled(): Boolean{
+        var correctness: Boolean = true
+        if(name_field.text.toString().equals("")){
+            name_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        if(last_name_field.text.toString().equals("")){
+            last_name_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        if(email_field.text.toString().equals("")){
+            email_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        if(login_field.text.toString().equals("")){
+            login_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        if(password_field.text.toString().equals("")){
+            password_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        if(password_repeat_field.text.toString().equals("")){
+            password_repeat_field.setError("Pole nie może być puste")
+            correctness = false
+        }
+        return correctness
+    }
+
+    override fun onBackPressed() {
+
     }
 }
